cbc73468211f6feb49694aaf266c41ef8f705525
Bastian <pechlerbastian@googlemail.com>
2022-12-04 17:00:18 +0100

    Data Parser adjusted to folds

--- DataParser.py
+++ DataParser.py
@@ -1,33 +1,62 @@
-import pandas
+import pandas as pd
 import numpy as np
+import librosa as lb
 
 class DataParser:
 
+    delimiter = None
     needed_columns = []
     target = []
 
-    def __init__(self, needed_columns, target):
+    def __init__(self, needed_columns, target, delimiter=','):
         self.needed_columns = needed_columns
         self.target = target
-        return
-
-    def load_data(self):
+        self.delimiter = delimiter
         return
 
     def load_audio_file(self):
         return
 
-    def parse_labels(self):
-        labels = pandas.read_csv('FeedbackExperiment/labels.csv')
-        header = np.asarray(labels.keys())
-        index_result = np.asarray([np.where(header == x) for x in self.target]).flatten()
-        y = np.asarray(labels.iloc[:, index_result].copy())
+    def parse_labels(self, fold_number=0):
+        develop = pd.read_csv('FeedbackExperiment-folds/'+str(fold_number)+'/dev.csv', sep=self.delimiter, usecols=['filename', 'VoiceRating'])
+        test = pd.read_csv('FeedbackExperiment-folds/'+str(fold_number)+'/test.csv', sep=self.delimiter, usecols=['filename', 'VoiceRating'])
+        train = pd.read_csv('FeedbackExperiment-folds/'+str(fold_number)+'/train.csv', sep=self.delimiter, usecols=['filename', 'VoiceRating'])
+        duration_frames = []
+        for file_name in train['filename']:
+            samples, sample_rate = lb.load('FeedbackExperiment/'+file_name, sr=16000) # TODO use hyperparam
+            duration = lb.get_duration(y=samples, sr=sample_rate)
+            duration_frames.append(int(duration*16000))
+        train.rename(columns={'VoiceRating': 'label'}, inplace=True)
+        train['duration_frames'] = duration_frames
+        train.to_csv(path_or_buf='FeedbackExperiment-folds/'+str(fold_number)+'/train_adjusted.csv')
 
+        duration_frames = []
+        for file_name in develop['filename']:
+            samples, sample_rate = lb.load('FeedbackExperiment/'+file_name, sr=16000) # TODO use hyperparam
+            duration = lb.get_duration(y=samples, sr=sample_rate)
+            duration_frames.append(int(duration*16000))
 
-        return y
+        develop.rename(columns={'VoiceRating': 'label'}, inplace=True)
+        develop['duration_frames'] = duration_frames
+        develop.to_csv(path_or_buf='FeedbackExperiment-folds/' + str(fold_number) + '/dev_adjusted.csv')
+
+        duration_frames = []
+        for file_name in test['filename']:
+            samples, sample_rate = lb.load('FeedbackExperiment/'+file_name, sr=16000) # TODO use hyperparam
+            duration = lb.get_duration(y=samples, sr=sample_rate)
+            duration_frames.append(int(duration*16000))
+
+        test.rename(columns={'VoiceRating': 'label'}, inplace=True)
+        test['duration_frames'] = duration_frames
+        test.to_csv(path_or_buf='FeedbackExperiment-folds/' + str(fold_number) + '/test_adjusted.csv')
+
+        return train, develop, test
+
+    def process_data(self):
+        return
 
     def load_data(self):
-        labels = pandas.read_csv('FeedbackExperiment/labels.csv')
+        labels = pd.read_csv('FeedbackExperiment/labels.csv')
         header = np.asarray(labels.keys())
         indices_parameters = np.asarray([np.where(header == x) for x in self.needed_columns]).flatten()
         index_result = np.asarray([np.where(header == x) for x in self.target]).flatten()
--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 #from deepspectrumlite import data_adapter
 #from deepspectrumlite import amplitude_to_db
 import json
+import logging
 import os
-
+import shutil
+from tensorboard.plugins.hparams import api as hp
 import numpy as np
 import tensorflow
 from deepspectrumlite import TransferBaseModel ,AugmentableModel
@@ -12,7 +14,17 @@ from deepspectrumlite import DataPipeline
 from deepspectrumlite import HyperParameterList
 from deepspectrumlite.lib.data.parser import ComParEParser
 
+METRIC_ACCURACY = 'accuracy'
+METRIC_PRECISION = 'precision'
+METRIC_RECALL = 'recall'
+METRIC_F_SCORE = 'f1_score'
+METRIC_MAE = 'mae'
+METRIC_RMSE = 'rmse'
+METRIC_MSE = 'mse'
+METRIC_LOSS = 'loss'
+
 if __name__ == '__main__':
+    log = logging.getLogger(__name__)
     parser = DataParser(['filename', 'PatientGroup', 'SubjectID', 'TrialGroup', 'TrialValence', 'ReappraisalID', 'Try', 'ProvidedFeedback'],
                         ['VoiceRating'])
     model_dir = 'model/'
@@ -22,17 +34,24 @@ if __name__ == '__main__':
     f = open('hyper_param.json')
     data = json.load(f)
     f.close()
-
     data_classes = data
 
     if data_classes is None:
         raise ValueError('no data classes defined')
-
+    tensorboard_initialised = False
     hyp_params = HyperParameterList(config_file_name='hyper_param.json')
     parser = DataParser(['filename', 'PatientGroup', 'SubjectID', 'TrialGroup', 'TrialValence', 'ReappraisalID', 'Try', 'ProvidedFeedback'],
-                        ['VoiceRating'])
-    for i in range(10):
-        tensorboard_dir = hyp_params.get_values(iteration_no=i)['tb_experiment']
+                        ['label'])
+    max_iterations = hyp_params.get_max_iteration()
+    log.info('Loaded hyperparameter configuration.')
+    log.info("Recognised combinations of settings: " + str(max_iterations) + "")
+    enable_cache = False
+    data_dir = 'FeedbackExperiment'
+    verbose = False
+    for i in range(max_iterations):
+        hparam_values = hyp_params.get_values(iteration_no=i)
+        hparam_values_tensorboard = hyp_params.get_values_tensorboard(iteration_no=i)
+        tensorboard_dir = hparam_values['tb_experiment']
 
         log_dir = os.path.join(model_dir, 'logs', tensorboard_dir)
 
@@ -40,29 +59,85 @@ if __name__ == '__main__':
 
         run_log_dir = os.path.join(log_dir, run_identifier)
         model_dir = os.path.join(model_dir, 'models', tensorboard_dir, run_identifier)
+        # delete old log
+        if os.path.isdir(run_log_dir):
+            shutil.rmtree(run_log_dir)
 
+        if not tensorboard_initialised:
+            # create tensorboard
+            with tensorflow.summary.create_file_writer(log_dir).as_default():
+                hp.hparams_config(
+                    hparams=hyp_params.get_hparams(),
+                    metrics=[hp.Metric(METRIC_ACCURACY, display_name='accuracy'),
+                             hp.Metric(METRIC_PRECISION, display_name='precision'),
+                             hp.Metric(METRIC_RECALL, display_name='unweighted recall'),
+                             hp.Metric(METRIC_F_SCORE, display_name='f1 score'),
+                             hp.Metric(METRIC_MAE, display_name='mae'),
+                             hp.Metric(METRIC_RMSE, display_name='rmse')
+                             ],
+                )
+                tensorboard_initialised = True
+        # Use a label file parser to load data
+        label_parser_key = hparam_values['label_parser']
+
+        train_data, devel_data, test_data = parser.parse_labels()
+
+
+        np.random.seed(0)
+        tensorflow.compat.v1.set_random_seed(0)
+
+        log.info('--- Starting trial: %s' % run_identifier)
+        log.info({h.name: hparam_values_tensorboard[h] for h in hparam_values_tensorboard})
+
+        log.info("Load data pipeline ...")
+        ########### TRAIN DATA ###########
         train_data_pipeline = DataPipeline(name='train_data_set', data_classes=data_classes,
-                                           enable_gpu=True, verbose=False, enable_augmentation=False,
-                                           hparams=hyp_params.get_values(iteration_no=i), run_id=i)
-        train_data = parser.parse_labels()
+                                           enable_gpu=True, verbose=True, enable_augmentation=False,
+                                           hparams=hparam_values, run_id=i)
         train_data_pipeline.set_data(train_data)
-        train_data_pipeline.set_filename_prepend(prepend_filename_str="FeedbackExperiment/Control/Kontrollgruppe/AM0767/Negative")
+        train_data_pipeline.set_filename_prepend(prepend_filename_str=data_dir)
         train_data_pipeline.preprocess()
         train_data_pipeline.up_sample()
-        train_dataset = train_data_pipeline.pipeline(cache=True)
+        train_dataset = train_data_pipeline.pipeline(cache=enable_cache)
 
-        print(train_data_pipeline.get_filenames())
+        ########### DEVEL DATA ###########
+        devel_data_pipeline = DataPipeline(name='devel_data_set', data_classes=data_classes,
+                                           enable_gpu=True, verbose=True, enable_augmentation=False,
+                                           hparams=hparam_values, run_id=i)
+        devel_data_pipeline.set_data(devel_data)
+        devel_data_pipeline.set_filename_prepend(prepend_filename_str=data_dir)
+        devel_dataset = devel_data_pipeline.pipeline(cache=enable_cache, shuffle=False, drop_remainder=False)
 
-   # X_train, y_train, X_val, y_val = tensorflow.keras.train_test_split(X, y, 0.2)
-        model = TransferBaseModel(hyp_params.get_hparams(),
-                    train_data_pipeline.get_model_input_shape(),
-                    run_dir=run_log_dir,
-                    data_classes=data_classes,
-                    use_ram=True,
-                    run_id=i,
-                    verbose=False)
-        #    model.compile()
-        model.compile(optimizer='Adam')
-        model.fit(X, y, batch_size=32, validation_split=0.2)
+        ########### TEST DATA ###########
+        test_data_pipeline = DataPipeline(name='test_data_set', data_classes=data_classes,
+                                          enable_gpu=True, verbose=True, enable_augmentation=False,
+                                          hparams=hparam_values, run_id=i)
+        test_data_pipeline.set_data(test_data)
+        test_data_pipeline.set_filename_prepend(prepend_filename_str=data_dir)
+        test_dataset = test_data_pipeline.pipeline(cache=enable_cache, shuffle=False, drop_remainder=False)
 
-        model.summary()
+        log.info("All data pipelines have been successfully loaded.")
+        log.info("Caching in memory is: " + str(enable_cache))
+
+        model_name = hparam_values['model_name']
+
+        available_ai_models = {
+            'TransferBaseModel': TransferBaseModel
+        }
+
+        if model_name in available_ai_models:
+            model = available_ai_models[model_name](hyp_params,
+                                                    train_data_pipeline.get_model_input_shape(),
+                                                    run_dir=run_log_dir,
+                                                    data_classes=data_classes,
+                                                    use_ram=True,
+                                                    run_id=i,
+                                                    verbose=verbose)
+
+            model.run(train_dataset=train_dataset,
+                      test_dataset=test_dataset,
+                      devel_dataset=devel_dataset,
+                      save_model=True,
+                      save_dir=model_dir)
+        else:
+            ValueError("Unknown model name: " + model_name)
